0eebc3a5945d658f53cc70d84fb6cfc6dc25909c
Andrei <andrdru@gmail.com>
2017-05-15 19:23:51 +0300

    test container timezone

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 /etc/init.d/exim4 start;
+
+# Set the timezone.
+if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
+    echo ${CONTAINER_TIMEZONE} >/etc/timezone && \
+    ln -sf /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime && \
+    dpkg-reconfigure -f noninteractive tzdata
+    echo "Container timezone set to: $CONTAINER_TIMEZONE"
+else
+    echo "Container timezone not modified"
+fi
+
 docker-php-entrypoint php-fpm;
